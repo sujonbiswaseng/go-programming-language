620ee56daf583a87f042792eb1795f24f545ed0b
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-05-01 10:09:26 +0600

    veriable

---
+++ variable.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var variable_name type = expression
+	var name string = "sujon"
+	// name = "sujon"
+	fmt.Println("string!", name)
+	var age int = 20
+	fmt.Println("int", age)
+	// variablename := value
+	fullname := "sujon biswas"
+	fmt.Println(fullname)
+
+	var a, b int = 1, 2
+	fmt.Println(a, "a", b, "b")
+}
